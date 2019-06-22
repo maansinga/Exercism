dd0bf738be7d2dd5bf16ecc684f66937ad79e34c
Sree Teja Simha G <Sreetejasimha.gemaraju@utdallas.edu>
2019-06-21 22:55:42 -0500

    Got the pascals triangle working

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,38 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle(u32);
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        PascalsTriangle(row_count)
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        match self.0{
+            0 => vec![],
+            1 => vec![vec![1]],
+            2 => vec![vec![1], vec![1,1]],
+            _ =>{
+                let mut pt2: Vec<Vec<u32>> = vec![vec![1], vec![1,1]];
+                for _ in 2..self.0{
+                    let last = pt2.last().unwrap();
+                    let mut new = vec![0];
+                    new.extend(last.iter());
+                    new.extend(vec![0].iter());
+                    pt2
+                        .push(
+                            new
+                                .windows(2)
+                                .map(|pair| {
+                                    match pair{
+                                        &[x, y] => x+y,
+                                        _ => panic!("This can never happen: Any other scenario")
+                                    }
+                                })
+                                .collect()
+                        );
+                }
+
+                pt2
+            }
+        }
     }
 }
