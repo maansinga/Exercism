6a2a1f2a42472ee0c7575820e25f604f658991d1
Sree Teja Simha G <Sreetejasimha.gemaraju@utdallas.edu>
2019-06-21 22:28:55 -0500

    Got simple linked list working!! Wohoo

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,55 +1,11 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-use core::borrow::{BorrowMut};
-
-type HeapNode<T> = Rc<RefCell<Box<Node<T>>>>;
+use core::borrow::BorrowMut;
 
 pub struct Node<T>{
     data: Option<T>,
-    next: Option<HeapNode<T>>
+    next: Option<Box<Node<T>>>
 }
 pub struct SimpleLinkedList<T> {
-    head: Option<HeapNode<T>>
-}
-
-impl<T> Node<T>{
-    pub fn make_rc_node(data: T)->Option<HeapNode<T>>{
-        Some(                                     //Option to make it none
-            Rc::new(                              //Option to let multiple references
-                RefCell::new(                //Option to allow interior mutability
-                    Box::new(                //Allocating memory on heap
-                        Node{                   //Actual data
-                            data: Some(data),
-                            next: None
-                        }
-                    )
-                )
-            )
-        )
-    }
-    pub fn len(&self)->usize{
-        match &self.next{
-            None => 1,
-            Some(rc_node)=>{
-                1 + rc_node
-                    .as_ref()
-                    .borrow_mut()
-                    .len()
-            }
-        }
-    }
-
-    pub fn append(&mut self, data: T){
-        match &self.next{
-            None => self.next = Node::make_rc_node(data),
-            Some(rc_node) => {
-                rc_node //Rc<RefCell<Box<Node<T>>>>
-                    .as_ref() //&RefCell<Box<Node<T>>>
-                    .borrow_mut() //&Box<Node<T>>
-                    .append(data);
-            }
-        }
-    }
+    head: Option<Box<Node<T>>>,
 }
 
 impl<T> SimpleLinkedList<T> {
@@ -58,52 +14,87 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn len(&self) -> usize {
-        match &self.head{
-            None => 0,
-            Some(rc_node) => {
-                rc_node
-                    .as_ref()
-                    .borrow_mut()
-                    .len()
+        let mut h = &self.head;
+        let mut count = 0;
+        loop{
+            match h {
+                None => break,
+                Some(box_node) => {
+                    h = &box_node.next;
+                    count += 1;
+                }
             }
         }
+        count
     }
 
     pub fn push(&mut self, _element: T) {
-        match &self.head{
-            None => self.head = Node::make_rc_node(_element),
-            Some(rc_node)=>{
-                rc_node
-                    .as_ref()
-                    .borrow_mut()
-                    .append(_element)
-            }
-        }
+        self.head = Some(
+            Box::new(
+                Node{
+                    data: Some(_element),
+                    next: self.head.take()
+                }
+            )
+        );
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!()
+        if self.head.is_none(){
+            None
+        }else{
+            let uhead = self.head.take().unwrap();
+            self.head = uhead.next;
+            uhead.data
+        }
+
     }
 
     pub fn peek(&self) -> Option<&T> {
-        unimplemented!()
+        if self.head.is_none() {
+            None
+        }else{
+            self.head.as_ref().unwrap().data.as_ref()
+        }
     }
 }
 
 impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(&self) -> SimpleLinkedList<T> {
-        unimplemented!()
+        let mut ret = SimpleLinkedList::new();
+        let mut head = &self.head;
+        while head.is_some() {
+            let uhead = head.as_ref().unwrap();
+            ret.push(uhead.data.clone().unwrap());
+            head = &uhead.next;
+        }
+
+        ret
     }
 }
 
 impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
     fn from(_item: &[T]) -> Self {
-        unimplemented!()
+        let mut ret = SimpleLinkedList::new();
+        for k in _item.iter(){
+            ret.push(k.clone())
+        }
+
+        ret
     }
 }
 
 impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
     fn into(self) -> Vec<T> {
-        unimplemented!()
+        let mut ret = vec![];
+        let mut head = self.head;
+        while head.is_some() {
+            let uhead = head.unwrap();
+            ret.push(uhead.data.unwrap());
+            head = uhead.next;
+        }
+
+        ret.reverse();
+        ret
     }
 }
