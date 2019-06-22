11d5b0408317ae2247f6fb4c7ad42aca544b070c
Sree Teja Simha G <Sreetejasimha.gemaraju@utdallas.edu>
2019-06-22 00:06:17 -0500

    pangram complete

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,17 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let mut punch_card: HashSet<char> = HashSet::from(
+        "abcdefghijklmnopqrstuvwxyz"
+            .chars()
+            .clone()
+            .collect()
+    );
+
+    for x in sentence.to_ascii_lowercase().chars() {
+        punch_card.remove(&x);
+    }
+
+    punch_card.is_empty()
 }
