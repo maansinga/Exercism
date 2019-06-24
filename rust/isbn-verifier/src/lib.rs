6cd9397b9014f3782fe7bea12aad72491e8c6fa3
Sree Teja Simha G <Sreetejasimha.gemaraju@utdallas.edu>
2019-06-23 20:14:41 -0500

    Different way to solve isbn verifier

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -1,45 +1,24 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let all_valid_chars_flag = isbn
-        .chars()
-        .all(|x| {
-            match x {
-                '0'..='9' | '-' | 'x' | 'X' => true,
-                _ => false
-            }
-        });
-
-    if !all_valid_chars_flag { return false; }
-
-    let has_x_in_right_place = if isbn.chars().any(|x| x == 'X') {
-        isbn.chars().last().unwrap() == 'X'
-    } else { true };
-
-    if !has_x_in_right_place { return false; }
-
-    let filtered = isbn
-        .chars()
-        .filter(|x| {
-            match x {
-                '0'..='9' | 'X' | 'x' => true,
-                _ => false
-            }
-        });
-
-    let post_filter_count_10 = filtered.clone().collect::<String>().len() == 10;
-
-    if !post_filter_count_10 { return false; }
-
-    filtered
-        .map(|x| {
-            match x {
-                '0'..='9' => x.to_digit(10).unwrap(),
-                'x' | 'X' => 10,
-                _ => 0
-            }
-        })
-        .zip((1..=10).rev())
-        .fold(0, |a, (x, y)| {
-            a + x * y
-        }) % 11 == 0
+    let mut place = (1..=10).rev().into_iter();
+    let mut accum = 0;
+    let mut sym_count = 0;
+    for c in isbn.chars(){
+        match c{
+            k @ '0'..='9' => {
+                accum = accum + k.to_digit(10).unwrap() * place.next().unwrap();
+                sym_count += 1;
+            },
+            'X'|'x' => {
+                if sym_count != 10 { return false; }
+                else{
+                    accum = accum + 10 * place.next().unwrap();
+                    sym_count += 1;
+                }
+            },
+            '-'=> continue,
+            _ => { return false; }
+        }
+    }
+    false
 }
